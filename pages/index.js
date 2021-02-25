70630637bed653b24ec212dcd067d995273a7d43
caio-valente <62984632+caio-valente@users.noreply.github.com>
2021-02-25 10:10:32 -0300

    initial webpage

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home () {
+  return <div>
+    <div>
+      <h1>Home!</h1>
+      <h2>Testing 2</h2>
+    </div>
+    <div>
+      <p>Testando uma página...123</p>
+    </div>
+  </div>
+}
+
+export default Home
